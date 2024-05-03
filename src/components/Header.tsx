06574d257504d42705aa24622820d1c727a90d50
msopiann <muhammadsopian452@gmail.com>
2024-05-04 02:24:50 +0700

    style: add instagram icon in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -143,6 +143,35 @@ export default function Header() {
             </svg>
           </Link>
         </li>
+
+        <li className="mr-5 text-xs shrink-0">
+          <Link
+            href="https://www.instagram.com/msopiann/"
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Instagram (opens in a new tab)"
+            title="Istagram"
+            className="block text-white hover:text-[#5CFF9D]"
+          >
+            <span className="sr-only">Istagram</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide lucide-instagram"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5" />
+              <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+              <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+            </svg>
+          </Link>
+        </li>
       </ul>
     </header>
   );
